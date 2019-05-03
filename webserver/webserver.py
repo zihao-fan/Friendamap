172aee795d0766b0026e401d9b993df188a2ee45
yi-gai <yi.gai0059@gmail.com>
2019-05-02 17:53:14 -0700

    update webserver

--- webserver/webserver.py
+++ webserver/webserver.py
@@ -4,7 +4,7 @@ import logging
 import mysql.connector
 import pymysql
 import requests
-import get_Closest
+#import get_Closest
 
 app = Flask(__name__)
 db = pymysql.connect(
@@ -100,7 +100,33 @@ def find_nearby_favorite():
 
     sorted_list = get_closest(results_list, user_place_dict)
     data = {"results": sorted_list}
-    resp = Response(json.dumps(data), status=status_code, mimetype='application/json')
+    resp = Response(json.dumps(data), status=201, mimetype='application/json')
+
+def get_closest(list_of_nearby, favorite_place):
+    """ score = 0 + no. pf duplicates in categories * 5 - price dollar sign digits difference * 2 - rating difference * 10 """
+    ref_rating = float(favorite_place["rating"]) # this is a float
+    ref_price_len = len(favorite_place["price"]) # this is the length of the dollar sign - an int
+    ref_categ = favorite_place["categories"] # this is a string!
+
+    for item in list_of_nearby:
+        score = 0
+        list_of_cat_words = item["categories"].split()
+        for word in list_of_cat_words:
+            if word in ref_categ:
+                score += 1
+        score = score * 5
+        score = score - 2 * abs(len(item["price"]) - ref_price_len)
+        score = score - 10 * abs(float(item["rating"]) - ref_rating)
+        item["score"] = score
+
+    for item in list_of_nearby:
+        return_list = []
+        return_list.append({"id": item["id"], "score": item["score"]})
+
+    return_list = sorted(return_list, key = lambda i: i["score"])
+    return return_list
+
+
 
 
 @app.route('/v1/liked', methods=["GET"])
